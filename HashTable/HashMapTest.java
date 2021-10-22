3b8277a27b952a16731b68f4b5de0492c29c942f
Smruti <lenkasmruti123@gmail.com>
2021-10-22 23:10:54 +0530

    Added: Uc2_HashTable

--- HashTable/HashMapTest.java
+++ HashTable/HashMapTest.java
@@ -4,19 +4,21 @@ import org.junit.Test;
 
 public class HashMapTest {
 	@Test
-	public void givenASentence_WhenWordsAREAddedToList_ShouldReturnWordFerquency() {		
-		String sentence = "To be or not to be";
+	public  void givenASentence_WhenWordAreAddToList_ShoudReturnWordFrequency () {
+		String sentence = "Paranoids are not paranoid because they are paranoid but because"
+				          + "they keep putting themselves deliberately into paranoid avoidable situations";
 		MyHashMap<String, Integer> myHashMap = new MyHashMap<>();
 		String[] words = sentence.toLowerCase().split(" ");
 		for (String word : words) {
 			Integer value = myHashMap.get(word);
-			if (value == null)value = 1;
+			if (value == null)
+				value = 1;
 			else
 				value = value + 1;
 			myHashMap.add(word, value);
 		}
-		int frequency = myHashMap.get("to");
+		int frequency = myHashMap.get("paranoid");
 		System.out.println(myHashMap);
-		System.out.println("Frequency of word(to) is " + frequency);
+		System.out.println("Frequency of word(paranoid) is " + frequency);
 		}
 }
